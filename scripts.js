496a14a3587bb90207fdef038572f8aa598d6d0c
Mikhail Kormyshov <kormyshov@gmail.com>
2014-05-02 20:17:11 +0800

    Add next_round and start sample_game

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
-var x;
+var count_round, current_round = -1;
 
 function start_game(){
 	document.getElementById("div_first_step").style.display = "none";
-	document.getElementById("div_round1").style.display = "block";
 
 	var str = document.getElementById("text_first_step").value;
 	eval(str);
-	x = _x;
-	print();
+	
+	count_round = Rounds;
+
+	next_round();
 }
 
-function print(){
-	alert(x);
+function next_round(){
+	current_round++;
+	if(current_round == count_round){
+		end_game();
+		return ;
+	}
+
+	document.getElementById("div_round").style.display = "block";
+	document.getElementById("div_round_header").innerHTML = "Раунд " + (current_round+1);
 }
