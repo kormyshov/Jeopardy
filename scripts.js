2047cd5c2b3bba9b86b459d307665bf5da5bfd79
Mikhail Kormyshov <kormyshov@gmail.com>
2014-05-06 13:56:17 +0800

    Add Round.print

--- scripts.js
+++ scripts.js
@@ -1,13 +1,45 @@
-var count_round, current_round = -1;
+var count_round = 0, current_round = -1;
+
+function Theme(){
+	
+	this.name;
+	this.questions = new Array();
+
+}
+
+function Round(){
+
+	this.themes = new Array();
+	this.points = new Array();
+
+	this.print = function(num){
+		document.getElementById("div_round_header").innerHTML = "Раунд " + num;
+
+		var table = document.getElementById("div_round_table");
+
+		for(var i=0; i<this.themes.length; i++){
+			table.getElementsByTagName("tr")[i].getElementsByTagName("td")[0].innerHTML = this.themes[i].name;
+			for(var j=0; j<this.points.length; j++)
+				table.getElementsByTagName("tr")[i].getElementsByTagName("td")[j+1].innerHTML = this.points[j];
+		}
+
+		//alert(table.getElementsByTagName("tr"));
+
+	}
+
+}
+
+var rounds = new Array();
 
 function start_game(){
 	document.getElementById("div_first_step").style.display = "none";
 
 	var str = document.getElementById("text_first_step").value;
-	eval(str);
-	
-	count_round = Rounds;
 
+	//alert("test1");
+	eval(str);
+	//alert("test2");
+	
 	next_round();
 }
 
@@ -19,5 +51,5 @@ function next_round(){
 	}
 
 	document.getElementById("div_round").style.display = "block";
-	document.getElementById("div_round_header").innerHTML = "Раунд " + (current_round+1);
+	rounds[current_round].print(current_round+1);
 }
